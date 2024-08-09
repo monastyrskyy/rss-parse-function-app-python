0577de3b52e8462d5d9df25ed420f3bc74c10d45
MAKSYM MONASTYRSKYY <t-systems.monastyrskyy@extaccount.com>
2024-08-09 16:26:18 +0200

    and the last func

--- function_app.py
+++ function_app.py
@@ -19,3 +19,12 @@ def mp3_download(myTimer: func.TimerRequest) -> None:
         logging.info('The timer is past due!')
 
     logging.info('Second func, just straight up in line?')
+
+@app.timer_trigger(schedule="0 0 5 * * *", arg_name="myTimer", run_on_startup=True,
+              use_monitor=False) 
+def reading_in_rss_and_writing_to_sql(myTimer: func.TimerRequest) -> None:
+    
+    if myTimer.past_due:
+        logging.info('The timer is past due!')
+
+    logging.info('Python timer trigger function executed.')
