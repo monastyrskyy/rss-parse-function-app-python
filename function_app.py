fa942a16569e35d87541df0cdb6e894e147d21f2
MAKSYM MONASTYRSKYY <t-systems.monastyrskyy@extaccount.com>
2024-08-09 16:11:45 +0200

    new function.
    how is this function at all differentiated from the first? and how is the name actually even working?

--- function_app.py
+++ function_app.py
@@ -10,3 +10,12 @@ def rss_refresh_daily(myTimer: func.TimerRequest) -> None:
         logging.info('The timer is past due!')
 
     logging.info('Start of Parallel dev in python. Hello.')
+
+@app.timer_trigger(schedule="0 2/20 * * * *", arg_name="myTimer", run_on_startup=True,
+              use_monitor=False) 
+def mp3_download(myTimer: func.TimerRequest) -> None:
+    
+    if myTimer.past_due:
+        logging.info('The timer is past due!')
+
+    logging.info('Second func, just straight up in line?')
